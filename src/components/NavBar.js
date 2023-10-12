5d9dd43bbd151c9939204e478af172a5d5b24955
Nageshwar  Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-10-12 23:22:29 +0530

    init

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,22 +2,38 @@ import React from 'react'
 import { headers } from '../../next.config'
 import Link from 'next/link'
 import Logo from './Logo'
+import { useRouter } from 'next/router'
+const CustomLink = ({ href, title,className="" }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span className={`h-[1px] inline-block  bg-dark absolute left-0 -bottom-0.5  
+      group-hover:w-full transition-[width]  ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}
+      >&nbsp;</span>
+
+
+
+
+    </Link>
+  )
+}
 
 const NavBar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex item-center justify-between'>
       <nav>
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/Projects'>Projects</Link>
-        <Link href='/articles'>Articles</Link>
-
+        <CustomLink  href="/" title="Home" className='mr-4' />
+        <CustomLink  href="/about" title="About" className='mx-4' /> 
+        <CustomLink  href="/projects" title="Projects"className='mx-4' />
+        <CustomLink  href="/ articles" title='Articles' className='ml-4'  />
 
       </nav>
       
       <nav>
 
-      <Link href="/" target='_blank' >Twitter</Link>
+      <Link href="/" target='_blank' >TwitterIcon</Link>
       <Link href="/" target='_blank' >Github</Link>
       <Link href="/" target='_blank' >Linkedin</Link>
       <Link href="/" target='_blank' >t</Link>
