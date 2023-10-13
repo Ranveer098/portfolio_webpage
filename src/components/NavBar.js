dc10070e70cd01da496c4030b54e1077666662b5
Nageshwar  Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-10-13 11:12:42 +0530

    init

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,53 +1,72 @@
-import React from 'react'
-import { headers } from '../../next.config'
-import Link from 'next/link'
-import Logo from './Logo'
-import { useRouter } from 'next/router'
-const CustomLink = ({ href, title,className="" }) => {
+import React from "react";
+import { headers } from "../../next.config";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import TwitterIcon,{ DribbbleIcon, GithubIcon, LinkedInIcon } from "./Icons";
+import Logo from "./Logo";
+import { motion } from "framer-motion";
+const CustomLink = ({ href, title, className = "" }) => {
   const router = useRouter();
 
   return (
     <Link href={href} className={`${className} relative group`}>
       {title}
-      <span className={`h-[1px] inline-block  bg-dark absolute left-0 -bottom-0.5  
-      group-hover:w-full transition-[width]  ease duration-300 ${router.asPath === href ? 'w-full' : 'w-0'}`}
-      >&nbsp;</span>
-
-
-
-
+      <span
+        className={`h-[1px] inline-block  bg-dark absolute left-0 -bottom-0.5  
+      group-hover:w-full transition-[width]  ease duration-300 ${
+        router.asPath === href ? "w-full" : "w-0"
+      }`}
+      >
+        &nbsp;
+      </span>
     </Link>
-  )
-}
+  );
+};
 
 const NavBar = () => {
   return (
-    <header className='w-full px-32 py-8 font-medium flex item-center justify-between'>
+    <header className="w-full px-32 py-8 font-medium flex item-center justify-between">
       <nav>
-        <CustomLink  href="/" title="Home" className='mr-4' />
-        <CustomLink  href="/about" title="About" className='mx-4' /> 
-        <CustomLink  href="/projects" title="Projects"className='mx-4' />
-        <CustomLink  href="/ articles" title='Articles' className='ml-4'  />
-
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/ articles" title="Articles" className="ml-4" />
       </nav>
-      
-      <nav>
 
-      <Link href="/" target='_blank' >TwitterIcon</Link>
-      <Link href="/" target='_blank' >Github</Link>
-      <Link href="/" target='_blank' >Linkedin</Link>
-      <Link href="/" target='_blank' >t</Link>
-      <Link href="/" target='_blank' >t</Link>
-      <Link href="/" target='_blank' >t</Link>
+      <nav className="flex items-center justy-center flex-wrap">
+        <motion.a href="https://twitter.com/nothing_more195" target="_blank"
+        className="w-6 mx-3"
+        whileHover={{y:-5}}
+        whileTap={{scale:0.9}}
+        >
+          <TwitterIcon />
+        </motion.a>
+        <motion.a href="https://github.com/Ranveer098" target="_blank"
+        className="w-6 mx-3"
+        whileHover={{y:-5}}
+        whileTap={{scale:0.9}}>
+          <GithubIcon/>
+        </motion.a>
+        <motion.a href="https://www.linkedin.com/in/nageshwar-yadav/" target="_blank"
+        className="w-6 mx-3"
+        whileHover={{y:-5}}
+        whileTap={{scale:0.9}}>
+          <LinkedInIcon />
+        </motion.a>
+        <motion.a href="/" target="_blank" 
+        className="w-6 ml-3"
+        whileHover={{y:-5}}
+        whileTap={{scale:0.9}}>
+          <DribbbleIcon/>
+        </motion.a>
+        
       </nav>
-      <div className='absolute left-[50%] top-2 transalte-x-[-50]'> <Logo /> </div>
-      
-
-
-
-
+      <div className="absolute left-[50%] top-2 transalte-x-[-50]">
+        {" "}
+        <Logo />{" "}
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
