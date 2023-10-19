b70acfe846a922af837c85174fdcc432886004fd
Nageshwar  Yadav <95100082+Ranveer098@users.noreply.github.com>
2023-10-20 00:03:56 +0530

    init

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,8 @@ import Layout from './Layout'
 
 const Footer = () => {
   return (
-    <footer className='w-full border-t-2 border-solid border-dark'>
-      <Layout className='py-8 items-center justify-between'>
+    <footer className='w-full border-t-2 border-solid border-dark font-medium text-lg'>
+      <Layout className='py-8 flex items-center justify-between'>
         <span>{new Date().getFullYear()}&copy;All Right Reserved.</span>
     
         <Link href="/" target={'_blank'} className='underline underline-offset-2'>Say Hello</Link>
