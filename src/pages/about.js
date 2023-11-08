28092190ba6bb448b1f344401fcb1991ef02fcaa
Ranveer098 <95100082+Ranveer098@users.noreply.github.com>
2023-11-08 23:12:08 +0530

    init

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,6 +2,8 @@ import Animatedtext from "@/components/Animatedtext";
 import React from "react";
 import Head from "next/head";
 import Layout from "@/components/Layout";
+import Image from "next/image";
+import profilePic from "../../public/images/profile/developer-pic-2.jpg";
 
 const about = () => {
   return (
@@ -13,17 +15,65 @@ const about = () => {
 
       <main className=" flex w-full flex-col items-center justify-center">
         <Layout className="pt-16">
-          <Animatedtext
-            text="Passion Fuels Purpose"/>
-        </Layout>
-        <div className="grid w-full grid-cols-8 gap-16">
-          <div className="col-span-3 flex-col items-start justify-start">
-            <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
-              
-              Biography
-            </h2>
+          <Animatedtext text="Passion Fuels Purpose" className="mb-16" />
+
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi, I'm CodeBucks, a web developer and UI/UX designer with a
+                passion for creating beautiful, functional, and user-centered
+                digital experiences. With 4 years of experience in the field. I
+                am always looking for new and innovative ways to bring my
+                clients' visions to life.
+              </p>
+
+              <p className=" my-4 font-medium">
+                I believe that design is about more than just making things look
+                pretty – it's about solving problems and creating intuitive,
+                enjoyable experiences for users.
+              </p>
+
+              <p className="font-medium">
+                Whether I'm working on a website, mobile app, or other digital
+                product, I bring my commitment to design excellence and
+                user-centered thinking to every project I work on. I look
+                forward to the opportunity to bring my skills and passion to
+                your next project.
+              </p>
+            </div>
+            <div
+              className="col-span-3  relative h-max rounded-2xl border-2 border-solid border-dark 
+            bg-light p-8"
+            >
+              <div className=" absolute top-0 -right-3 -z-10 w-[102%] h-[103%]  rounded-[2rem] bg-dark" />
+              <Image
+                src={profilePic}
+                alt="Ranveer"
+                className=" w-full h-auto rounded-2xl"
+              />
+            </div>
+            <div className=" col-span-2 flex flex-col items-end justify-between">
+              <div>
+                <span>50+</span>
+                <h2> satisfied clients</h2>
+              </div>
+
+              <div>
+                <span>50+</span>
+                <h2> Projects Complete</h2>
+              </div>
+
+              <div>
+                <span>50+</span>
+                <h2>years of experience</h2>
+              </div>
+
+            </div>
           </div>
-        </div>
+        </Layout>
       </main>
     </>
   );
