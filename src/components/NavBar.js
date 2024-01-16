aec2f2d18dde1cca0c2592b5ccdfddd32a219615
Ranveer098 <95100082+Ranveer098@users.noreply.github.com>
2024-01-16 16:25:54 +0530

    init

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -40,91 +40,161 @@ const NavBar = () => {
   };
 
   return (
-    <header className="w-full px-32 py-8 font-medium flex items-center justify-between  dark:text-light ">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between  dark:text-light relative ">
       <button
-        className="flex flex-col justify-center items-center"
+        className=" flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}
-
       >
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm -translate-y-0.5     
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm      
    ${isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"}
   
   `}
         ></span>
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm my-0.5
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5
   ${isOpen ? "opacity-0" : "opacity-100"}
   
   
   `}
         ></span>
-       <span className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm   
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm   
    ${isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"}
   
-  `}></span>
+  `}
+        ></span>
       </button>
 
-      <nav>
-        <CustomLink href="/" title="Home" className="mr-4" />
-        <CustomLink href="/about" title="About" className="mx-4" />
-        <CustomLink href="/projects" title="Projects" className="mx-4" />
-        {/* <CustomLink href="/ articles" title="Articles" className="ml-4" /> */}
-      </nav>
+      <div className="w-full flex justify-between items-center lg:hidden">
+        <nav>
+          <CustomLink href="/" title="Home" className="mr-4" />
+          <CustomLink href="/about" title="About" className="mx-4" />
+          <CustomLink href="/projects" title="Projects" className="mx-4" />
+          {/* <CustomLink href="/ articles" title="Articles" className="ml-4" /> */}
+        </nav>
 
-      <nav className="flex items-center justify-center  flex-wrap">
-        <motion.a
-          href="https://twitter.com/nothing_more195"
-          target="_blank"
-          className="w-6 mx-3"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <TwitterIcon />
-        </motion.a>
-        <motion.a
-          href="https://github.com/Ranveer098"
-          target="_blank"
-          className="w-6 mx-3"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <GithubIcon />
-        </motion.a>
+        <nav className="flex items-center justify-center  flex-wrap">
+          <motion.a
+            href="https://twitter.com/nothing_more195"
+            target="_blank"
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <TwitterIcon />
+          </motion.a>
+          <motion.a
+            href="https://github.com/Ranveer098"
+            target="_blank"
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <GithubIcon />
+          </motion.a>
 
-        <motion.a
-          href="https://www.linkedin.com/in/nageshwar-yadav/"
-          target="_blank"
-          className="w-6 mx-3"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <LinkedInIcon />
-        </motion.a>
+          <motion.a
+            href="https://www.linkedin.com/in/nageshwar-yadav/"
+            target="_blank"
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <LinkedInIcon />
+          </motion.a>
 
-        <motion.a
-          href="/"
-          target="_blank"
-          className="w-6 ml-3"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-        >
-          <DribbbleIcon />
-        </motion.a>
+          <motion.a
+            href="/"
+            target="_blank"
+            className="w-6 ml-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <DribbbleIcon />
+          </motion.a>
 
-        <button
-          onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className={`ml-3 flex items-center justify-center rounded-full p-1
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-3 flex items-center justify-center rounded-full p-1
             ${mode === "light" ? "bg-dark text-light" : "bg-light  text-dark"}
             `}
-        >
-          {mode === "dark" ? (
-            <SunIcon className={"fill-dark"} />
-          ) : (
-            <MoonIcon className={"fill-dark"} />
-          )}
-        </button>
-      </nav>
+          >
+            {mode === "dark" ? (
+              <SunIcon className={"fill-dark"} />
+            ) : (
+              <MoonIcon className={"fill-dark"} />
+            )}
+          </button>
+        </nav>
+      </div>
+
+
+      <div className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2  -translate-y-1/2 -translate-x-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
+        <nav>
+          <CustomLink href="/" title="Home" className="mr-4" />
+          <CustomLink href="/about" title="About" className="mx-4" />
+          <CustomLink href="/projects" title="Projects" className="mx-4" />
+          {/* <CustomLink href="/ articles" title="Articles" className="ml-4" /> */}
+        </nav>
+
+        <nav className="flex items-center justify-center  flex-wrap">
+          <motion.a
+            href="https://twitter.com/nothing_more195"
+            target="_blank"
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <TwitterIcon />
+          </motion.a>
+          <motion.a
+            href="https://github.com/Ranveer098"
+            target="_blank"
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <GithubIcon />
+          </motion.a>
+
+          <motion.a
+            href="https://www.linkedin.com/in/nageshwar-yadav/"
+            target="_blank"
+            className="w-6 mx-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <LinkedInIcon />
+          </motion.a>
+
+          <motion.a
+            href="/"
+            target="_blank"
+            className="w-6 ml-3"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            <DribbbleIcon />
+          </motion.a>
+
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`ml-3 flex items-center justify-center rounded-full p-1
+            ${mode === "light" ? "bg-dark text-light" : "bg-light  text-dark"}
+            `}
+          >
+            {mode === "dark" ? (
+              <SunIcon className={"fill-dark"} />
+            ) : (
+              <MoonIcon className={"fill-dark"} />
+            )}
+          </button>
+        </nav>
+      </div>
+
+
+
       <div className="absolute left-[50%] top-2 transalte-x-[-50%]">
         <Logo />
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -34,7 +34,7 @@ module.exports = {
 
 
     }, 
-    screens:{
+    
       screens: {
         "2xl": { max: "1535px" },
         // => @media (max-width: 1535px) { ... }
@@ -55,7 +55,7 @@ module.exports = {
         // => @media (max-width: 479px) { ... }
     },
     
-    }
+    
   },
   plugins: [],
 };
