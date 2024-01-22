5bdaad17f33475613d48995c18d5cfd32f91896d
Ranveer098 <95100082+Ranveer098@users.noreply.github.com>
2024-01-22 14:13:19 +0530

    init

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -92,7 +92,7 @@ const NavBar = () => {
       <div className="w-full flex justify-between items-center lg:hidden">
         <nav>
           <CustomLink href="/" title="Home" className="mr-4" />
-          <CustomLink href="/about" title="About" className="mx-4" />
+          <CustomLink href="/About" title="About" className="mx-4" />
           <CustomLink href="/projects" title="Projects" className="mx-4" />
           {/* <CustomLink href="/ articles" title="Articles" className="ml-4" /> */}
         </nav>
@@ -159,7 +159,7 @@ const NavBar = () => {
       className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2  -translate-y-1/2 -translate-x-1/2 bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32">
       <nav className=" flex items-center flex-col justify-center">
         <CustomMobileLink href="/" title="Home" className="" toggle={handleClick} />
-        <CustomMobileLink href="/about" title="About" className="" toggle={handleClick}/>
+        <CustomMobileLink href="/About" title="About" className="" toggle={handleClick}/>
         <CustomMobileLink href="/projects" title="Projects" className="" toggle={handleClick}/>
         {/* <CustomLink href="/ articles" title="Articles" className="ml-4" /> */}
       </nav>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,8 @@ import "@/styles/globals.css";
 import { Montserrat } from "next/font/google";
 import Head from "next/head";
 import Footer from "@/components/Footer";
+import { AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 
 
@@ -12,6 +14,7 @@ const montserrat = Montserrat({
 });
 
 export default function App({ Component, pageProps }) {
+  const router=useRouter();
   return (
     <>
       <Head>
@@ -20,7 +23,12 @@ export default function App({ Component, pageProps }) {
       </Head>
       <main className={`${montserrat.variable} font-mont bg-light dark:bg-dark w-full min-h-screen`}>
         <NavBar />
-        <Component {...pageProps} />
+        
+
+      <AnimatePresence mode="wait"> 
+
+        <Component  key={router.aspath}{...pageProps} />
+        </AnimatePresence>
         <Footer/>
         
       </main>
